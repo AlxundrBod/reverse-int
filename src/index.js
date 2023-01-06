7634d0b3752123ec499bbd537affbe52587e5092
AlxundrBod <talismanariona@gmail.com>
2023-01-06 17:05:37 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let strN = String(n);
+    let i = strN.length - 1;
+    let result = '';
+    if (n < 0) {
+        while (i > 0) {
+            result = `${result}${Number(strN[i])}`;
+            i = i - 1;
+        }
+        return result;
+    } else {
+        while (i >= 0) {
+            result = `${result}${Number(strN[i])}`;
+            i = i - 1;
+        }
+        return result;
+    }
+};
